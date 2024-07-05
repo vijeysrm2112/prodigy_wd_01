1b2660996412c570b38bebdb8960b5a98a20fef3
vijeysrm2112 <vjprinceofficial@gmail.com>
2024-07-05 20:09:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', () => {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.style.backgroundColor = '#555';
+    } else {
+        navbar.style.backgroundColor = '#333';
+    }
+});
